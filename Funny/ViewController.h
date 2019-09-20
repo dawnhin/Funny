b1cc28fd693f8063ac2db53d6e5fa0435f997a62
黎铭轩 <limingxuan@limingxuandemacbook-pro.local>
2019-09-20 21:19:14 +0800

    Initial Commit

---
+++ Funny/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Funny
+//
+//  Created by 黎铭轩 on 20/9/2019.
+//  Copyright © 2019 黎铭轩. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
